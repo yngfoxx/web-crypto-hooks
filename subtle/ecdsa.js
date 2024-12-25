ddb4b0be7595db028b98058deda36bb125e6f743
Stephen Osunrinde <47150226+yngfoxx@users.noreply.github.com>
2024-12-25 19:52:21 +0100

    fix: remove typescript typings

--- subtle/ecdsa.js
+++ subtle/ecdsa.js
@@ -64,7 +64,7 @@ async function useECDSA(algorithm = {name:'ECDSA',hash:'SHA-256'}) {
      * @param {string} ivHex
      * @returns
      */
-    const decrypt = async (cipherHex: string, ivHex: string) => {
+    const decrypt = async (cipherHex, ivHex) => {
         return await crypto.subtle.decrypt(
             { name: 'AES-CBC', iv: hex2buf(ivHex) },
             tmpKeys!.privateKey,
